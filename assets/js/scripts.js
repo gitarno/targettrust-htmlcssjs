1f2707b9f9052ff564405b802b291976ebb8e24d
Arno Roldao Junior <arno.junior@getnet.com.br>
2017-05-13 17:34:10 -0300

    Para unificar o metodo do botão

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,37 +1,40 @@
 "use strict";
 
-function soma(numeroA, numeroB){
+function soma(numeroA, numeroB) {
     return numeroA + numeroB;
 }
-function subtrair(numeroA, numeroB){
+function subtrair(numeroA, numeroB) {
     return numeroA - numeroB;
 }
-function multiplicar(numeroA, numeroB){
+function multiplicar(numeroA, numeroB) {
     return numeroA * numeroB;
 }
-function dividir(numeroA, numeroB){
+function dividir(numeroA, numeroB) {
     return numeroA / numeroB;
 }
 
-function ativarBotao(){
+function ativarBotao($botao,$metodo) {
+    $botao.addEventListener("click", function () {
+        var numeroA = Number(campoA.value),
+            numeroB = Number(campoB.value);
+
+        document.getElementById("resultado").innerHTML = $metodo(numeroA, numeroB); 
+    });
 
 }
 
-window.onload = function(){
+window.onload = function () {
     var botaoSOMAR = document.getElementById("somar"),
         botaoSUBTRAIR = document.getElementById("subtrair"),
         botaoMULTIPLICAR = document.getElementById("multiplicar"),
         botaoDIVIDIR = document.getElementById("dividir");
 
-    var campoA = document.getElementById("numeroA"),
-        campoA = document.getElementById("numeroA");
+    var campoA = document.getElementById("campoA"),
+        campoB = document.getElementById("campoB");
 
-    botaoSOMAR.addEventListener("click",function(){
-        var numeroA = Number(campoA.value),
-            numeroB = Number(campoB.value);
-        document.getElementById("resultado").innerHTML = soma(numeroA,numeroB; 
-    });
-
-    
+    ativarBotao(botaoSOMAR,soma);
+    ativarBotao(botaoSUBTRAIR,subtrair);
+    ativarBotao(botaoMULTIPLICAR,multiplicar);
+    ativarBotao(botaoDIVIDIR,dividir);
 };
 
