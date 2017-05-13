2a9361188f8be386b8dbfd4abac24b134e422215
Arno Roldao Junior <arno.junior@getnet.com.br>
2017-05-13 16:57:44 -0300

    PEgando os valores e fazendo o elemento BUTTON monstrar o resultado no objeto ID=RESULTADO

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,15 +13,25 @@ function dividir(numeroA, numeroB){
     return numeroA / numeroB;
 }
 
+function ativarBotao(){
+
+}
+
 window.onload = function(){
     var botaoSOMAR = document.getElementById("somar"),
         botaoSUBTRAIR = document.getElementById("subtrair"),
         botaoMULTIPLICAR = document.getElementById("multiplicar"),
         botaoDIVIDIR = document.getElementById("dividir");
 
+    var campoA = document.getElementById("numeroA"),
+        campoA = document.getElementById("numeroA");
 
     botaoSOMAR.addEventListener("click",function(){
-        console.log("Olá");
+        var numeroA = Number(campoA.value),
+            numeroB = Number(campoB.value);
+        document.getElementById("resultado").innerHTML = soma(numeroA,numeroB; 
     });
+
+    
 };
 
