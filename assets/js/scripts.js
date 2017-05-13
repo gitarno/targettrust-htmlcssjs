4c4da0dff5636d18c83c8d81a00805ca2b4703e5
Arno Roldao Junior <arno.junior@getnet.com.br>
2017-05-13 16:53:10 -0300

    para habilitar outras operações

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,43 +1,24 @@
 "use strict";
-var meunome,
-    minhaIdade, 
-    meuSexo,
-    habilitado;
-
-meunome = "Arno Roldão Junior";
-minhaIdade = 60;
-meuSexo = null;
-habilitado = false;
-
-// Comentário em linha
-//alert(meunome);
-/*
- Comentário em bloco
-*/
-// console.log(meunome, minhaIdade, meuSexo);
-// console.log("Olá, " + meunome);
-// console.log(2+2); // SOMA
-// console.log(2-2); // SUBTRAÇÃO
-// console.log(2/2); // DIVISÃO
-// console.log(2%2); // RESTO ou MOD
-// var soma = 2+2;
-// console.log("SOMA: "+ soma );
 
 function soma(numeroA, numeroB){
     return numeroA + numeroB;
 }
+function subtrair(numeroA, numeroB){
+    return numeroA - numeroB;
+}
+function multiplicar(numeroA, numeroB){
+    return numeroA * numeroB;
+}
+function dividir(numeroA, numeroB){
+    return numeroA / numeroB;
+}
 
-// function generica(){
-//    console.log(arguments);
-// };
-
-// generica("A");
-// generica("B",2,4);
-// generica("c",true,false);
-
-//gjhdgadgashjd
 window.onload = function(){
-    var botaoSOMAR = document.getElementById("somar");
+    var botaoSOMAR = document.getElementById("somar"),
+        botaoSUBTRAIR = document.getElementById("subtrair"),
+        botaoMULTIPLICAR = document.getElementById("multiplicar"),
+        botaoDIVIDIR = document.getElementById("dividir");
+
 
     botaoSOMAR.addEventListener("click",function(){
         console.log("Olá");
