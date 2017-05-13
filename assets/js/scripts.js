3708d502ed666b82620bfd70fee25571918d9a90
Arno Roldao Junior <arno.junior@getnet.com.br>
2017-05-13 15:09:17 -0300

    função simples

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,30 @@
 "use strict";
+var meunome,
+    minhaIdade, 
+    meuSexo,
+    habilitado;
 
+meunome = "Arno Roldão Junior";
+minhaIdade = 60;
+meuSexo = null;
+habilitado = false;
 
+// Comentário em linha
+//alert(meunome);
+/*
+ Comentário em bloco
+*/
+// console.log(meunome, minhaIdade, meuSexo);
+// console.log("Olá, " + meunome);
+// console.log(2+2); // SOMA
+// console.log(2-2); // SUBTRAÇÃO
+// console.log(2/2); // DIVISÃO
+// console.log(2%2); // RESTO ou MOD
+// var soma = 2+2;
+// console.log("SOMA: "+ soma );
 
+function soma(){
+    console.log("O Resultado é: "+ (2+2) );
+}
+
+soma();
