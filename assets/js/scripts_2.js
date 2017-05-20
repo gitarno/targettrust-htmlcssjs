103af4281b639e9f62e0889d89367450fad89734
Arno Roldao Junior <arno.junior@getnet.com.br>
2017-05-20 14:08:28 -0300

    Manipulando comportamentos padrões de objetos com eventos de click.

--- assets/js/scripts_2.js
+++ assets/js/scripts_2.js
@@ -3,41 +3,49 @@
 var nome = "Arno Roldão",
     data = 20;
 
+var objeto = {
+    nome: "Arno Roldão",
+    sobrenome: "Junior",
+    idade: 30,
+    email: "arno@advg.com.br",
+    falar: function () {
+        console.log("Olá!");
+    }
+};
+var meuArray = [
+    nome,
+    objeto,
+    "adsasda",
+    12311
+];
 
-window.onload = function(){
-    console.log("Bem vindo!" + nome);
-    console.log("Atenção não digite nada nessa área, que você não saiba!")
-
-
-    var objeto = {
-            nome: "Arno Roldão",
-            sobrenome: "Junior",
-            idade: 30,
-            email: "arno@advg.com.br",
-            falar: function(){
-                console.log("Olá!");
-            }
-        };
-    
-    console.log(objeto.nome);
-
+window.onload = function () {
+    // console.log("Bem vindo!" + nome);
+    // console.log("Atenção não digite nada nessa área, que você não saiba!")
+    // console.log(objeto.nome);
     objeto.falar();
+    // console.log(objeto.nome.length);
+    // console.log(objeto.nome[objeto.nome.length - 1]);
+    // console.log(objeto.email.indexOf("@"));
+    var header = document.getElementById("menu-principal");
+        // console.log("Manipulando:" + header.id);
+        header.style.background = "rgba(44, 62, 80, 0.7)";
+        header.className += " active";
 
-    var meuArray = [
-        nome,
-        objeto,
-        "adsasda",
-        12311
-    ];
+    var menu = {
+        logotipo: header.children[0],
+        itens: header.children[1].children[0].children
+    };
 
-    console.log(objeto.nome.length);
-    console.log(objeto.nome[objeto.nome.length-1]);
-    console.log(objeto.email.indexOf("@"));
+    console.log(menu.itens[0].children[0]);
+
+    menu.itens[0].children[0].addEventListener("click",function(event){
+        event.preventDefault();
+        console.log(event);
+        window.location.href = menu.itens[0].children[0].href;
+    });
 
 
-    var menu = document.getElementById("menu-principal");
-        console.log("Manipulando:" + menu.id);
-        menu.style.background = "rgba(44, 62, 80, 0.7)";
-        menu.className += " active";
-        
+    menu.logotipo.innerHTML = "Olá, " + objeto.nome;
+
 }
